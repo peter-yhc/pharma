744b2b274b819194b0bc43c8e459df6d7063106e
Peter Chen <peter.chen2@mail.mcgill.ca>
2019-11-05 08:48:18 -0800

    Add dummy sql data.

---
+++ dummy_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO `Patient` (`username`, `password`, `name`, `age`, `sex`, `address`, `city`, `country`, `postalCode`, `phone1`, `phone2`, `emergencyContactName`, `emergencyContactPhone`) VALUES
+('Patient04', 'Patient04', 'Buckethead', 22, 'M', 'Nottingham Lace', 'City?', 'Soothsayer', '40591', '0999555222', NULL, 'GoldenEye', '072222111'),
+('Patient01', 'Patient01', 'User001', 55, 'M', '0122 Cloud District', 'White Run', 'Skyrim', '50903', '9975137', '496842', 'Wife001', '031234567'),
+('Patient02', 'Patient02', 'The Killers', 22, 'M', 'Miss Atomic Bomb', 'City?', 'Read My Mind', '555777', '65616378', '6518745', 'Shot At The Night', '02689186'),
+('Patient03', 'Patient03', 'Kings Of Leon', 25, 'M', 'Use Somebody', 'City?', 'Pyro', '95971', '5165789', '6489135', 'King Of The Rodeo', '65489797');
